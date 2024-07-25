eaf3830a98b597b91dab0daf4b5485525483c1b8
Kevin Upton <k_upton@live.com>
2024-07-26 00:30:11 +1000

    updated deploy script

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -1,12 +1,9 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {Script, stdJson} from "forge-std/Script.sol";
-
 import "./Base.s.sol";
 
 contract DeployScript is BaseScript {
-    using stdJson for string;
 
     function run() public {
         vm.startBroadcast(deployer);
