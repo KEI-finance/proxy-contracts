16c7373595eb710617d55c22130961c6c93b6850
Kevin Upton <k_upton@live.com>
2024-10-26 05:05:54 +1100

    added gts

--- utils/get-deployment-address.ts
+++ utils/get-deployment-address.ts
@@ -1,6 +1,9 @@
-import deployments from "../deployments.json";
-import { getDeployment as getForgeDeployment } from "forge-utils";
+import deployments from '../deployments.json';
+import {getDeployment as getForgeDeployment} from 'forge-utils';
 
-export const getDeploymentAddress = (chainId: string | number, env?: string) => {
-    return getForgeDeployment(deployments, chainId, env)
-}
+export const getDeploymentAddress = (
+  chainId: string | number,
+  env?: string,
+) => {
+  return getForgeDeployment(deployments, chainId, env);
+};
