ae17a9d3c6d23da6c72259d8bd270090ce36e843
Kevin Upton <k_upton@live.com>
2024-10-26 16:53:10 +1100

    updated base script

--- script/Base.s.sol
+++ script/Base.s.sol
@@ -1,12 +1,15 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity =0.8.20;
 
+import {stdJson} from "forge-std/StdJson.sol";
 import {BaseDeployScript} from "@kei.fi/testing-lib/BaseDeployScript.sol";
 
 abstract contract BaseScript is BaseDeployScript {
     using stdJson for string;
 
-    struct DeployConfig {}
+    struct DeployConfig {
+      // TODO complete this
+    }
 
     DeployConfig internal config;
 
